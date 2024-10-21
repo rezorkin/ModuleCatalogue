eb15dd6a53d7cfd2b4fb276a89ca1f871b5accb5
rezorkin <22ap5813@barton.ac.uk>
2024-10-21 09:22:56 +0100

    Add project files.

---
+++ ModuleCatalogue2/Pages/ModuleList.razor.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace ModuleCatalogue2.Pages
+{
+    public partial class ModuleList
+    {
+        private List<Module>? modules;
+        protected override async Task OnInitializedAsync()
+        {
+            modules = await Http.GetFromJsonAsync<List<Module>>("modulesInformation.json");
+        }
+        public class Module
+        {
+            public string? name { get; set; }
+            public string? description { get; set; }
+            public string? duration { get; set; }
+            public int creditScore { get; set; }
+            public string? moduleCoordinator { get; set; }
+        }
+    }
+}
---
+++ ModuleCatalogue2/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace ModuleCatalogue2
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            await builder.Build().RunAsync();
+        }
+    }
+}
